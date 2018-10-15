51793f55c791e81b185f80927ff53e3dc218d502
dybik08 <dybik08@gmail.com>
2018-10-15 21:50:15 +0200

    Modified product model

--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -2,8 +2,13 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const Product = new Schema({
-   name: { type: String, required: true},
-   price: { type: Number, required: true}
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    description: {type: String},
+    createdAt: {type: Number, default: Date.now},
+    updatedAt: {type: Number, default: Date.now},
+    amount: {type: Number, required: true, default: 0},
+    picture: {type: String, required: true, default: '../img/mikasa-pilka.jpg'}
 });
 
 export default mongoose.model('Product', Product);
