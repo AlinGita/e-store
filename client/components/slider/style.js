84a817b0d08ec34f9e0208503b59b77c5e8b1293
dybik08 <dybik08@gmail.com>
2018-10-21 20:35:21 +0200

    Add navbar and slider

---
+++ client/components/slider/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+export const SliderContainer = styled.div`
+  position: relative;
+  height: 500px;
+`
+export const SliderImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: opacity linear 1s;
+  opacity: ${props => props.fade ? 0 : 1};
+`
