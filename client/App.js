25f46952cdfee9c5710b49adccbdd916b0fa99a9
dybik08 <dybik08@gmail.com>
2018-10-28 12:32:12 +0100

    Refractored styles.js files and add Dashboard container

--- client/App.js
+++ client/App.js
@@ -10,25 +10,37 @@ import About from './containers/About';
 import Home from './containers/Home';
 import Contact from './containers/Contact';
 import Product from './containers/Product';
+import Dashboard from './containers/Dashboard';
 
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import ShoppingCart from './components/shoppingCart';
 
+
+const Client = () => (
+    <React.Fragment>
+        <Navbar/>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/products/:id" component={Product}/>
+        </Switch>
+        <Footer/>
+        <Basket/>
+    </React.Fragment>
+);
+const Admin = (props) => (
+    <Route path={props.match.path} component={Dashboard}/>
+);
+
 export default () => {
     return (
         <Router>
-            <main>
-                <Navbar/>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/about" component={About}/>
-                    <Route path="/contact" component={Contact}/>
-                    <Route path="/products/:id" component={Product}/>
-                </Switch>
-                <Footer/>
-                <ShoppingCart/>
-            </main>
+            <Switch>
+                <Route path="/dashboard" component={Admin}/>
+                <Route path="/" component={Client}/>
+            </Switch>
         </Router>
     )
 }
