194bb3de19629a657a6cee46e3c5115e69bbff1f
dybik08 <dybik08@gmail.com>
2018-11-01 19:52:21 +0100

    Update dashboard/products page

--- client/containers/Dashboard/Products/index.js
+++ client/containers/Dashboard/Products/index.js
@@ -1,9 +1,101 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSizes } from 'actions/sizesActions';
+import {
+    fetchProducts,
+    updateProduct,
+    deleteProduct,
+    createProductAvailability,
+    updateProductAvailability,
+    deleteProductAvailability
+} from 'actions/productsActions';
+import { fetchCategories } from 'actions/categoriesActions';
+import AvailabilityEditor from 'components/editors/AvailabilityEditor';
+import ProductEditor from 'components/editors/ProductEditor';
+import Expander from 'components/Expander';
 
-export default class Products extends Component {
+class Products extends Component {
+    componentDidMount() {
+        this.props.fetchSizes();
+        this.props.fetchProducts();
+        this.props.fetchCategories();
+    }
+    onSubmitProduct = (product) => {
+        this.props.updateProduct(product._id, product);
+    }
+    onSubmitAvailability = (availability) => {
+        const { _id, product, size, amount } = availability;
+        if(_id) {
+            this.props.updateProductAvailability(_id, size, amount);
+        } else {
+            this.props.createProductAvailability(product, size, amount);
+        }
+    }
+    deleteProduct = (productId) => {
+        this.props.deleteProduct(productId);
+    }
+    deleteProductAvailability = (availabilityId, productId) => {
+        this.props.deleteProductAvailability(availabilityId, productId);
+    }
+
     render() {
+        const { sizes } = this.props.sizes;
+        const { products } = this.props.products;
+        const { categories } = this.props.categories;
+
         return (
-            <h1>Products</h1>
+            <div>
+                <ul>
+                    { products &&
+                    Object.values(products).map(product => (
+                        <li key={product._id}>
+                            <Expander
+                                head={(
+                                    <div>
+                                        <h1>{product.name}</h1>
+                                        <p>{product.description}</p>
+                                    </div>
+                                )}
+                                body={(
+                                    <div>
+                                        <button onClick={() => this.deleteProduct(product._id)}>Delete</button>
+                                        <ProductEditor
+                                            { ...product }
+                                            categories={Object.values(categories)}
+                                            onSubmit={this.onSubmitProduct}
+                                        />
+                                        {
+                                            product.availability.map(availability => (
+                                                <div>
+                                                    <AvailabilityEditor
+                                                        { ...availability }
+                                                        product={product._id}
+                                                        sizes={Object.values(sizes)}
+                                                        onSubmit={this.onSubmitAvailability}
+                                                    />
+                                                    <button onClick={() => this.deleteProductAvailability(availability._id, product._id)}>Delete</button>
+                                                </div>
+                                            ))
+                                        }
+                                        <AvailabilityEditor
+                                            product={product._id}
+                                            sizes={Object.values(sizes)}
+                                            onSubmit={this.onSubmitAvailability}
+                                        />
+                                    </div>
+                                )}
+                            />
+                        </li>
+                    ))
+                    }
+                </ul>
+            </div>
         )
     }
 }
+
+const mapStateToProps = ({ products, sizes, categories }) => ({ products, sizes, categories });
+export default connect(mapStateToProps, {
+    fetchProducts, updateProduct, deleteProduct, createProductAvailability, updateProductAvailability, deleteProductAvailability,
+    fetchCategories, fetchSizes })(Products);
+
