c0aef6691caf4dc8c258ac884221f6111d97d31b
dybik08 <dybik08@gmail.com>
2018-11-18 17:01:42 +0100

    Create delivery model,service, controller

---
+++ server/models/DeliveryModel.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const Delivery = Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true, default: 0 },
+    createdAt: { type: Number, default: Date.now },
+    updatedAt: { type: Number, default: Date.now }
+})
+export default mongoose.model('Delivery', Delivery);
