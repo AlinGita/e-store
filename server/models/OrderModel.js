c11c5f3fd7609260540c60c265597beec1d1a8a0
dybik08 <dybik08@gmail.com>
2018-11-18 19:21:01 +0100

    Add order model on server side

---
+++ server/models/OrderModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const Order = new Schema({
+    payment: { type: Schema.Types.ObjectId, required: true, ref: 'Payment' },
+    delivery: { type: Schema.Types.ObjectId, required: true, ref: 'Delivery' },
+    products: [{
+        product: { type: Schema.Types.ObjectId, required: true, ref: 'Product'},
+        size: { type: Schema.Types.ObjectId, required: true, ref: 'Size' },
+        amount: { type: Number, required: true, default: 1 }
+    }],
+    price: { type: Number, required: true, default: 0 },
+    client: { type: Object, required: true },
+    address: { type: Object, required: true },
+    createdAt: { type: Number, default: Date.now }
+});
+
+export default mongoose.model('Order', Order);
