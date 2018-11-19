cdedd23abd2d6b70539d11a290d3c1e5627a1111
dybik08 <dybik08@gmail.com>
2018-11-19 15:31:30 +0100

    Create pre-save function that calculate total order price and add it to order's model

--- server/models/OrderModel.js
+++ server/models/OrderModel.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import Product from '../models/ProductModel';
+import Payment from '../models/PaymentModel';
+import Delivery from '../models/DeliveryModel';
 
 const Schema = mongoose.Schema;
 const Order = new Schema({
@@ -13,6 +16,33 @@ const Order = new Schema({
     client: { type: Object, required: true },
     address: { type: Object, required: true },
     createdAt: { type: Number, default: Date.now }
+});
+
+Order.pre('save', async function(next) {
+    try {
+        const productsIds = this.products.map(product => product.product);
+        const products = await Product.find({_id: {$in: productsIds}});
+
+        const productsPrice = this.products.reduce((acc, product) => {
+            const pr = products.find(item => {
+                const itemid = mongoose.Types.ObjectId(item._id);
+                const prodid = mongoose.Types.ObjectId(product.product);
+                return itemid.equals(prodid);
+            });
+            const productPrice = pr ? pr.price : 0;
+            return acc + (product.amount * productPrice);
+        }, 0);
+
+        const payment = await Payment.findById(this.payment);
+        const paymentPrice = payment.price;
+        const delivery = await Delivery.findById(this.delivery);
+        const deliveryPrice = delivery.price;
+        this.price = Number(productsPrice) + Number(paymentPrice) + Number(deliveryPrice);
+        next();
+    } catch (e) {
+        console.log(e);
+        next(e);
+    }
 });
 
 export default mongoose.model('Order', Order);
