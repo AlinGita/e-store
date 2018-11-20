6d1497637d695e805c6d5cd08796d2d0b8551da5
dybik08 <dybik08@gmail.com>
2018-11-20 22:04:50 +0100

    Add option to change delivery icon

--- server/models/DeliveryModel.js
+++ server/models/DeliveryModel.js
@@ -6,6 +6,8 @@ const Delivery = Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true, default: 0 },
     createdAt: { type: Number, default: Date.now },
-    updatedAt: { type: Number, default: Date.now }
-})
+    updatedAt: { type: Number, default: Date.now },
+    icon: { type: String, required: true, default: '/img/delivery/placeholder.png' },
+});
+
 export default mongoose.model('Delivery', Delivery);
