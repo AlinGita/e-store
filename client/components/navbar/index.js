4675aadeb4db09e2a339f850a2d13f65b5149df0
dybik08 <dybik08@gmail.com>
2018-11-21 20:26:23 +0100

    Update navbar styles

--- client/components/navbar/index.js
+++ client/components/navbar/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Wrapper from 'blocks/Wrapper';
 import {
     Branding,
     Header,
+    Container,
     Navigation,
+    Toggler
 } from './styles';
 
 import { openShoppingCart } from 'actions/shoppingCart';
@@ -14,20 +15,28 @@ class Navbar extends Component {
     render() {
         return (
             <Header>
-                <Wrapper>
-                    <Branding to="/">
-                        <img src="http://cdn.shopify.com/s/files/1/0188/3162/t/4/assets/logo.png?10389605073256300014" alt="branding"/>
+                <Container>
+                    <Toggler type="checkbox"></Toggler>
+                    <Branding>
+                        <Link to="/">
+                            <img src="/img/logo.png" alt="Branding"/>
+                        </Link>
                     </Branding>
                     <Navigation>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
+                        <Navigation.Menu>
                             <li><Link to="/products">Products</Link></li>
+                            <li><Link to="#">Categories</Link></li>
                             <li><Link to="/about">About</Link></li>
                             <li><Link to="/contact">Contact</Link></li>
-                        </ul>
+                        </Navigation.Menu>
+                        <Navigation.Basket>
+                            <button onClick={() => this.props.openBasket(true)}>
+                                <span className="fas fa-shopping-cart"></span>
+                                <span>Basket</span>
+                            </button>
+                        </Navigation.Basket>
                     </Navigation>
-                    <button onClick={() => this.props.openShoppingCart(true)}>Toggle shopping cart</button>
-                </Wrapper>
+                </Container>
             </Header>
         )
     }
