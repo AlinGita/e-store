2e8340571a02cc1ddfca4d52bc32e09f48ac99e6
dybik08 <dybik08@gmail.com>
2018-11-21 20:39:00 +0100

    Move layout style to seperate file and update layout style

--- client/containers/Dashboard/index.js
+++ client/containers/Dashboard/index.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
-import {
-    Switch,
-    Route,
-    Link
-} from 'react-router-dom';
-import styled from 'styled-components';
-const Container = styled.div`
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: 200px auto;
-`
+import { Switch, Route } from 'react-router-dom';
+
+import { Layout } from './styles';
 import Navigation from './Navigation';
 import Overview from './Overview';
 import Products from './Products';
@@ -23,19 +15,23 @@ import Deliveries from './Deliveries';
 export default class Dashboard extends Component {
     render() {
         return (
-            <Container>
-                <Navigation />
-                <Switch>
-                    <Route exact path={this.props.match.path} component={Overview}/>
-                    <Route path={`${this.props.match.path}/products/new`} component={ProductsNew}/>
-                    <Route path={`${this.props.match.path}/products`} component={Products}/>
-                    <Route path={`${this.props.match.path}/orders`} component={Orders}/>
-                    <Route path={`${this.props.match.path}/categories`} component={Categories}/>
-                    <Route path={`${this.props.match.path}/sizes`} component={Sizes}/>
-                    <Route path={`${this.props.match.path}/payments`} component={Payments}/>
-                    <Route path={`${this.props.match.path}/deliveries`} component={Deliveries}/>
-                </Switch>
-            </Container>
+            <Layout>
+                <Layout.Left>
+                    <Navigation />
+                </Layout.Left>
+                <Layout.Right>
+                    <Switch>
+                        <Route exact path={this.props.match.path} component={Overview}/>
+                        <Route path={`${this.props.match.path}/products/new`} component={ProductsNew}/>
+                        <Route path={`${this.props.match.path}/products`} component={Products}/>
+                        <Route path={`${this.props.match.path}/orders`} component={Orders}/>
+                        <Route path={`${this.props.match.path}/categories`} component={Categories}/>
+                        <Route path={`${this.props.match.path}/sizes`} component={Sizes}/>
+                        <Route path={`${this.props.match.path}/payments`} component={Payments}/>
+                        <Route path={`${this.props.match.path}/deliveries`} component={Deliveries}/>
+                    </Switch>
+                </Layout.Right>
+            </Layout>
         )
     }
 }
