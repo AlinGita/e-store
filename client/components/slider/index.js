622faebc6801938b1e119323360e00cbcb30fb51
dybik08 <dybik08@gmail.com>
2018-11-21 22:31:01 +0100

    Update slider component and it's style

--- client/components/slider/index.js
+++ client/components/slider/index.js
@@ -2,22 +2,40 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
     SliderContainer,
-    SliderImage
+    SliderImage,
+    Indicators,
+    Indicator
 } from './style';
+
 export default class Slider extends Component {
     state = {
         current: 0
     };
+
     componentDidMount() {
-        this.interval = setInterval(() => {
-            const { current } = this.state;
-            const next = current + 1 >= this.props.images.length ? 0 : current + 1;
-            this.setState({ current: next })
-        }, 5000);
+        this.setTimer(5000);
     }
+
     componentWillUnmount() {
-        clearInterval(this.interval);
+        if(this.interval)
+            clearInterval(this.interval);
     }
+
+    setTimer = delay => {
+        if (this.interval)
+            clearInterval(this.interval);
+        this.interval = setInterval(() => {
+            const {current} = this.state;
+            const next = current + 1 >= this.props.images.length ? 0 : current + 1;
+            this.setState({current: next})
+        }, delay)
+    };
+
+    setCurrent = index => {
+        this.setState({ current: index });
+        this.setTimer(5000);
+    };
+
     render() {
         const { images } = this.props;
         return (
@@ -27,6 +45,13 @@ export default class Slider extends Component {
                     <SliderImage key={index} src={image} alt={image} fade={index!==this.state.current}/>
                 ))
                 }
+                <Indicators>
+                    { images &&
+                    images.map((image, index) => (
+                        <Indicator key={index} onClick={() => this.setCurrent(index)} selected={index === this.state.current}/>
+                    ))
+                    }
+                </Indicators>
             </SliderContainer>
         )
     }
