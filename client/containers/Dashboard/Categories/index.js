ab0502f969d45382ce72d45c72f6a2cef2ecffb5
dybik08 <dybik08@gmail.com>
2018-11-22 12:35:06 +0100

    Update categories style

--- client/containers/Dashboard/Categories/index.js
+++ client/containers/Dashboard/Categories/index.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import {
     fetchCategories,
     createCategory,
     updateCategory,
     deleteCategory
 } from 'actions/categoriesActions';
+
 import CategoryEditor from './CategoryEditor';
 import Expander from 'components/Expander';
+
+import { Cards } from './styles';
+import Button from 'blocks/Button';
+
 class Categories extends Component {
     state = {
         name: '',
         description: '',
-    }
+    };
+
     componentDidMount = async () => {
         this.props.fetchCategories();
-    }
+    };
+
     changeName = e => {
         this.setState({ name: e.target.value })
-    }
+    };
+
     changeDescription = e => {
         this.setState({ description: e.target.value });
-    }
+    };
+
     submitCategory = e => {
         e.preventDefault();
         const { name, description } = this.state;
         this.props.createCategory({ name, description });
-    }
+    };
+
     changeCategory = async (id, name, description) => {
         this.props.updateCategory(id, { name, description });
-    }
+    };
 
     render() {
         const {name, description} = this.state;
@@ -41,26 +52,28 @@ class Categories extends Component {
                     <input type="text" placeholder="Description" value={description} onChange={this.changeDescription}/>
                     <button type="submit" onClick={this.submitCategory}>Submit</button>
                 </form>
-                <ul>
+                <Cards>
                     {
                         Object.values(categories).map(category => (
-                            <li key={category._id}>
+                            <Cards.Item key={category._id}>
                                 <Expander
-                                    head={(<h1>Name: {category.name}, Description: {category.description}</h1>)}
+                                    head={(
+                                        <React.Fragment>
+                                            <h3>{category.name}</h3>
+                                            <small>{category._id}</small>
+                                        </React.Fragment>
+                                    )}
                                     body={(
                                         <div>
-                                            <CategoryEditor name={category.name} description={category.description}
-                                                            onSubmit={(name, description) => this.changeCategory(category._id, name, description)}/>
-                                            <button onClick={() => this.props.deleteCategory(category._id)}>Delete
-                                                category
-                                            </button>
+                                            <CategoryEditor name={category.name} description={category.description} onSubmit={(name, description) => this.changeCategory(category._id, name, description)}/>
+                                            <Button type="danger" onClick={() => this.props.deleteCategory(category._id)}>Delete</Button>
                                         </div>
                                     )}
                                 />
-                            </li>
+                            </Cards.Item>
                         ))
                     }
-                </ul>
+                </Cards>
             </div>
         )
     }
